121e8ce936296cb352492a4838c3ab3a60568d77
lijianqiang <newphper@126.com>
2022-06-07 15:22:27 +0800

    0.1.0

--- src/main/java/com/bytehonor/server/demo/spring/SpringDemoServerApplication.java
+++ src/main/java/com/bytehonor/server/demo/spring/SpringDemoServerApplication.java
@@ -3,11 +3,14 @@ package com.bytehonor.server.demo.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bytehonor.sdk.server.bytehonor.annotation.SpringBootStandard;
+
+@SpringBootStandard
 @SpringBootApplication
 public class SpringDemoServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoServerApplication.class, args);
+    }
 
 }
