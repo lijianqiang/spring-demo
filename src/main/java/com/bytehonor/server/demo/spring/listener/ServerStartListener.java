9de36f12a92b5b2cd8cbb1745e91812332dcf9da
lijianqiang <343891954@qq.com>
2022-07-07 21:38:21 +0800

    ServerStartListener Component

--- src/main/java/com/bytehonor/server/demo/spring/listener/ServerStartListener.java
+++ src/main/java/com/bytehonor/server/demo/spring/listener/ServerStartListener.java
@@ -1,8 +1,11 @@
 package com.bytehonor.server.demo.spring.listener;
 
+import org.springframework.stereotype.Component;
+
 import com.bytehonor.sdk.server.spring.start.SpringServerListener;
 import com.bytehonor.server.demo.spring.scheduler.SchedulerStarter;
 
+@Component
 public class ServerStartListener implements SpringServerListener {
 
     @Override
