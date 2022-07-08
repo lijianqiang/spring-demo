9c1a9be25f99402fe2eba0770a653f49633423f1
lijianqiang <343891954@qq.com>
2022-07-08 20:32:25 +0800

    0.1.5

--- src/main/java/com/bytehonor/server/demo/spring/scheduler/SchedulerStarter.java
+++ src/main/java/com/bytehonor/server/demo/spring/scheduler/SchedulerStarter.java
@@ -1,14 +1,14 @@
 package com.bytehonor.server.demo.spring.scheduler;
 
 import com.bytehonor.sdk.server.spring.scheduler.SpringScheduler;
-import com.bytehonor.server.demo.spring.scheduler.handler.RedisTaskHandler;
+import com.bytehonor.server.demo.spring.scheduler.handler.RedisTaskLocker;
 import com.bytehonor.server.demo.spring.scheduler.plan.PrintLogPlan;
 
 public class SchedulerStarter {
 
     public static void init() {
 
-        SpringScheduler.start(new RedisTaskHandler());
+        SpringScheduler.start(new RedisTaskLocker());
 
         SpringScheduler.add(new PrintLogPlan());
     }
