2afcaaf1f5da8be0b53c9b48a40eddb2a784751f
lijianqiang <newphper@126.com>
2022-07-08 10:48:00 +0800

    0.1.3

--- src/main/java/com/bytehonor/server/demo/spring/listener/ServerStartListener.java
+++ src/main/java/com/bytehonor/server/demo/spring/listener/ServerStartListener.java
@@ -2,7 +2,7 @@ package com.bytehonor.server.demo.spring.listener;
 
 import org.springframework.stereotype.Component;
 
-import com.bytehonor.sdk.server.spring.start.SpringServerListener;
+import com.bytehonor.sdk.server.spring.listener.SpringServerListener;
 import com.bytehonor.server.demo.spring.scheduler.SchedulerStarter;
 
 @Component
