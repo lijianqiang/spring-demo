d26c11e0954525b650a8e5a4d0fc54b55535da3c
lijianqiang <newphper@126.com>
2022-07-12 15:42:44 +0800

    index

--- src/main/java/com/bytehonor/server/demo/spring/controller/IndexController.java
+++ src/main/java/com/bytehonor/server/demo/spring/controller/IndexController.java
@@ -4,37 +4,29 @@ import java.time.LocalDateTime;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bytehonor.sdk.define.spring.result.StringResultVO;
 import com.bytehonor.sdk.lang.spring.util.LocalDateTimeUtils;
+import com.bytehonor.sdk.server.spring.SpringServer;
 
 @RestController
 public class IndexController {
 
     private static final Logger LOG = LoggerFactory.getLogger(IndexController.class);
 
-    @Value("${spring.application.name}")
-    private String applicationName;
-
-    @Value("${server.port}")
-    private Integer serverPort;
-
     @RequestMapping("/")
     public StringResultVO actionIndex() {
-        String server = new StringBuilder(applicationName).append(":").append(serverPort).toString();
-        LOG.info("hello, {}", server);
-        return StringResultVO.of("hello world");
+        LOG.info("hello, {}", SpringServer.id());
+        return new StringResultVO("hello world");
     }
 
     @RequestMapping("/time")
     public StringResultVO actionTime() {
-        LocalDateTime ldt = LocalDateTime.now();
-        String format = LocalDateTimeUtils.format(ldt);
-        LOG.info("time:{}, timestamp:{}", format, LocalDateTimeUtils.toTimestamp(ldt));
-        return StringResultVO.of(format);
+        String format = LocalDateTimeUtils.format(LocalDateTime.now());
+        LOG.info("time:{}", format);
+        return new StringResultVO(format);
     }
 
 }
