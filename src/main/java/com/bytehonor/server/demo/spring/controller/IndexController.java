db577a1246f3372ff42dc9308e7e2af7c1f864d4
lijianqiang <newphper@126.com>
2022-08-05 17:31:54 +0800

    event

--- src/main/java/com/bytehonor/server/demo/spring/controller/IndexController.java
+++ src/main/java/com/bytehonor/server/demo/spring/controller/IndexController.java
@@ -4,17 +4,23 @@ import java.time.LocalDateTime;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bytehonor.sdk.define.spring.response.DataString;
 import com.bytehonor.sdk.lang.spring.util.TimeFormatUtils;
+import com.bytehonor.sdk.message.server.event.EventPayload;
+import com.bytehonor.sdk.message.server.producer.ServerEventProducer;
 import com.bytehonor.sdk.server.spring.SpringServer;
 
 @RestController
 public class IndexController {
 
     private static final Logger LOG = LoggerFactory.getLogger(IndexController.class);
+
+    @Autowired
+    private ServerEventProducer serverEventProducer;
 
     @RequestMapping("/")
     public DataString actionIndex() {
@@ -27,6 +33,14 @@ public class IndexController {
         String format = TimeFormatUtils.format(LocalDateTime.now());
         LOG.info("time:{}", format);
         return new DataString(format);
+    }
+
+    @RequestMapping("/test")
+    public DataString actionTest() {
+        LOG.info("actionTest");
+        EventPayload payload = EventPayload.build("actionTest");
+        serverEventProducer.produce(payload);
+        return DataString.ok();
     }
 
 }
