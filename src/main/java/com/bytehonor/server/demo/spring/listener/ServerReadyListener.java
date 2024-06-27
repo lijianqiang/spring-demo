560f813f5fbcfe1d4718698343b39ae0951af814
lijianqiang <newphper@126.com>
2024-06-27 10:30:45 +0800

    spring-server-sdk

--- src/main/java/com/bytehonor/server/demo/spring/listener/ServerReadyListener.java
+++ src/main/java/com/bytehonor/server/demo/spring/listener/ServerReadyListener.java
@@ -2,7 +2,7 @@ package com.bytehonor.server.demo.spring.listener;
 
 import org.springframework.stereotype.Component;
 
-import com.bytehonor.sdk.server.spring.listener.ReadyListener;
+import com.bytehonor.sdk.server.spring.web.listener.ReadyListener;
 import com.bytehonor.sdk.starter.jdbc.JdbcConfig;
 import com.bytehonor.server.demo.spring.event.SpringEventStarter;
 import com.bytehonor.server.demo.spring.scheduler.SchedulerStarter;
